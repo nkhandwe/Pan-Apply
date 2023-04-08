50497c1e2444d4bdace414463e953c93f4c1d190
Narendra Khandwe <nkhandwe@gmail.com>
2023-04-08 12:53:55 +0530

    front page folder created

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,9 @@ use App\Http\Controllers\PermissionController;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/ 
-Route::get('/', function () { return view('home'); });
+*/
+Route::get('/', function () {
+	return view('frontend/home'); });
 
 
 Route::get('login', [LoginController::class,'showLoginForm'])->name('login');
